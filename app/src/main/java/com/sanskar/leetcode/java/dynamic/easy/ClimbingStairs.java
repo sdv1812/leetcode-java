e8a3acabaa5772acf233698ab89b89568604a9f3
sanskar <sanskar.verma90@gmail.com>
2023-03-16 14:01:05 +1100

    Climbing stairs

---
+++ app/src/main/java/com/sanskar/leetcode/java/dynamic/easy/ClimbingStairs.java
@@ -0,0 +1,30 @@
+package com.sanskar.leetcode.java.dynamic.easy;
+
+public class ClimbingStairs {
+
+    // this is not suitable for larger numbers
+    public int climbStairs(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return climbStairs(n - 1) + climbStairs(n - 2);
+    }
+
+    public int climbStairsDP(int n) {
+        if (n < 2) {
+            return n;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        ClimbingStairs c = new ClimbingStairs();
+        System.out.println(c.climbStairs(1));
+    }
+}
